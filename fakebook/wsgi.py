1445201f8b498fcfa4fe278c117ff44fe05b9d5c
Naresh <naresh.k@credr.com>
2016-04-22 17:59:47 +0530

    updated wsgi

--- fakebook/wsgi.py
+++ fakebook/wsgi.py
@@ -7,8 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fakebook.settings")
+import os, sys, site
+
+SITE_DIR = '/home/ubuntu/fakebook/'
+site.addsitedir(SITE_DIR)
+sys.path.append(SITE_DIR)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'fakebook.settings'
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
